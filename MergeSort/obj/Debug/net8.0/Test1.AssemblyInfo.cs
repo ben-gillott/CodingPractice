c472c7f56bdca36e02fe04bddfacfde1cfd6c685
Ben Gillott <52325353+ben-gillott@users.noreply.github.com>
2024-05-28 14:00:19 -0700

    gitignore

--- MergeSort/obj/Debug/net8.0/Test1.AssemblyInfo.cs
+++ MergeSort/obj/Debug/net8.0/Test1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32518048096d27b771ebe9a1fe87c433351d5774")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f228453ba30226276228a77573d0f151fadde987")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
