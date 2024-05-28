32518048096d27b771ebe9a1fe87c433351d5774
Ben Gillott <52325353+ben-gillott@users.noreply.github.com>
2024-05-28 12:25:07 -0700

    Init

---
+++ MergeSort/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//https://code.visualstudio.com/docs/csharp/get-started
+//Command palette - new .net - console app template
+
+Console.WriteLine("STARTING PROGRAM");
+Main([]);
+
+
+static void Main(string[] args)
+{
+    Console.WriteLine("Hello World!");
+}
