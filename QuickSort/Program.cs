455f7763e719b46faa8c730c2201e6ba01db0ace
Ben Gillott <52325353+ben-gillott@users.noreply.github.com>
2024-05-30 14:07:23 -0700

    rm ds store, quicksort prog

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -7,11 +7,11 @@ Test();
 
 void Test(){
     Console.WriteLine("Starting Test");
-    int[] t1 = new int[]{3,2,4,1,5,2,1,4,6,3,6,1,8,23,4,32,99,-1};
-    int[] sorted = quickSort(t1);
-
-    Console.WriteLine("input:" + arrayToString(t1));
-    Console.WriteLine("output:" + arrayToString(sorted));
+    int[] t1 = new int[]{2,4,1,5,3};
+    Console.WriteLine("in:" + arrayToString(t1));
+    
+    quickSort(ref t1, 0 , t1.Length-1);
+    Console.WriteLine("out:" + arrayToString(t1));
 }
 
 string arrayToString(int[] a){
@@ -29,8 +29,43 @@ string arrayToString(int[] a){
 
 // ===========
 
-int[] quickSort(int[] arr){
-    //Recursive approach
+void quickSort(ref int[] arr, int leftBound, int rightBound){
+    Console.WriteLine("\t Sorting [" + leftBound+ ", " + rightBound + "] " + arrayToString(arr));
 
-    return [];
+    if(rightBound - leftBound <= 0){
+        return;
+    }
+    //Select the pivot value  - say last element
+    int n = arr.Length - 1;
+
+    int pivot = arr[n-1];
+
+    //Iterate from left and right - swapping high values right, and low to the left
+    int i = 0; //i will be set to the leftmost value greater than pivot
+    int j = n-1; //j will be set to the rightmost value less than pivot
+
+    while(i < j){
+        if(arr[i] > pivot && arr[j] < pivot){
+            //Swap
+            int temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
+            i++;
+            j--;
+        }
+        else if(arr[i] < pivot){
+            i++;
+        }
+        else{
+            j++;
+        }
+    }
+
+    //Move the pivot to it's correct positon
+    arr[n-1] = arr[j];
+    arr[j] = pivot;
+
+    //Recurse left and right
+    quickSort(ref arr, leftBound, j-1);
+    quickSort(ref arr, j+1, rightBound);
 }
