4657cf23cf8ae2a76ee6654cbe163836b2afcf8b
Ben Gillott <52325353+ben-gillott@users.noreply.github.com>
2024-05-29 21:50:29 -0700

    Quicksort start

---
+++ QuickSort/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+//https://code.visualstudio.com/docs/csharp/get-started
+//Command palette - new .net - console app template
+
+Console.WriteLine("STARTING PROGRAM");
+Test();
+
+void Test(){
+    Console.WriteLine("Starting Test");
+    int[] t1 = new int[]{3,2,4,1,5,2,1,4,6,3,6,1,8,23,4,32,99,-1};
+    int[] sorted = quickSort(t1);
+
+    Console.WriteLine("input:" + arrayToString(t1));
+    Console.WriteLine("output:" + arrayToString(sorted));
+}
+
+string arrayToString(int[] a){
+    string s = "[";
+
+    for(int i = 0; i < a.Length; i++){
+        s = s + a[i];
+        if(i < a.Length-1){
+            s = s + ", ";
+        }
+    }
+    s = s + "]";
+    return s;
+}
+
+// ===========
+
+int[] quickSort(int[] arr){
+    //Recursive approach
+
+    return [];
+}
